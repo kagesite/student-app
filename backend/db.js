162d6731977f8ec46cedaa2e3ddee7c1310a8ad9
Kage Saluone <ksaluone17@gmail.com>
2025-03-03 09:40:13 -0700

    Created pool module. Should connect once you add in your local connection string

--- backend/db.js
+++ backend/db.js
@@ -1,1 +1,10 @@
+const { Pool } = require('pg');
 require('dotenv').config();
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+})
+
+pool.connect()
+    .then(() => console.log(`Connected to PostGreSQL`))
+    .catch(err => console.error(`Database connection error:`, err));
