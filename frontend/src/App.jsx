7db3ae073681b20ff3ab7d540f7b205733fd0c08
Kage Saluone <ksaluone17@gmail.com>
2025-03-04 10:04:04 -0700

    Adjusted file imports

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import UserSignup from './components/StudentSignup'
+import StudentSignup from './pages/StudentSignup';
 import StudentLogin from './pages/StudentLogin';
 import AdminLogin from './pages/AdminLogin';
 import "./styles/index.css"
@@ -9,7 +9,7 @@ import "./styles/index.css"
 function App() {
     return (
         <Routes>
-            <Route path="/" element={<UserSignup />} />
+            <Route path="/" element={<StudentSignup />} />
 
             <Route path="student-login" element={<StudentLogin />} />
             <Route path="admin-login" element={<AdminLogin />} />
