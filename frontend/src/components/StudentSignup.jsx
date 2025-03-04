a7cfcca1632ba231cfd72a072a26707edb96b5b9
Kage Saluone <ksaluone17@gmail.com>
2025-03-04 08:57:40 -0700

    Changed UserSignup to StudentSignup

---
+++ frontend/src/components/StudentSignup.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from './Header'
+
+function UserSignup() {
+
+    return (
+        <div>
+            <Header />
+            <h2>Student Signup Page</h2>
+            <div>
+                
+            </div>
+        </div>
+    )
+}
+
+export default UserSignup
