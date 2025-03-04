fe65b3fd2faacca40c73440cbe507fc9e04a1a14
Kage Saluone <ksaluone17@gmail.com>
2025-03-04 08:16:03 -0700

    Created components dir

---
+++ frontend/src/components/UserSignup.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function UserSignup() {
+
+    
+    
+    return (
+        <div>
+            <h1>StudentApp</h1>
+        </div>
+    )
+}
+
+export default UserSignup
