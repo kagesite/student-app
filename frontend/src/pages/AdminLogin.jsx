26a964381e5877f6b685ad055b0451d8a9e68ce0
Kage Saluone <ksaluone17@gmail.com>
2025-03-04 08:55:22 -0700

    Created pages dir with Student and admin login pages

---
+++ frontend/src/pages/AdminLogin.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../components/Header'
+
+function AdminLogin() {
+    return (
+        <>
+            <Header />
+            <h2>Admin Login Page</h2>
+        </>
+    )
+}
+
+export default AdminLogin
---
+++ frontend/src/pages/StudentLogin.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../components/Header'
+
+function StudentLogin() {
+    return (
+        <>
+            <Header />
+            <h2>Student Login Page</h2>
+        </>
+    )
+}
+
+export default StudentLogin
