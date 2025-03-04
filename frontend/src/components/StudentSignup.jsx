a637ca3d9c74488fb6bee9b0b5cbe1c09a618fdf
Kage Saluone <ksaluone17@gmail.com>
2025-03-04 09:25:09 -0700

    Started to create basic form

--- frontend/src/components/StudentSignup.jsx
+++ frontend/src/components/StudentSignup.jsx
@@ -6,9 +6,31 @@ function UserSignup() {
     return (
         <div>
             <Header />
-            <h2>Student Signup Page</h2>
-            <div>
-                
+            <div className="form-container">
+                <h2>Student Signup Page</h2>
+                <form>
+                    <div>
+                        <label>Name:</label>
+                        <input 
+                            type="text" 
+                            placeholder='Name'
+                        />
+                    </div>
+                    <div>
+                        <label>Email:</label>
+                        <input 
+                            type="email" 
+                            placeholder='Email'
+                        />
+                    </div>
+                    <div>
+                        <label>Password:</label>
+                        <input 
+                            type="password" 
+                            placeholder='Password'
+                        />
+                    </div>
+                </form>
             </div>
         </div>
     )
