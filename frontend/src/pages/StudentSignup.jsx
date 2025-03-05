4c27ab5985f4f5643bb631a03777970362f14cd4
Kage Saluone <ksaluone17@gmail.com>
2025-03-04 22:07:50 -0700

    Created basic handleSubmit function. This now connects the frontend inputs to the backend server on submit

--- frontend/src/pages/StudentSignup.jsx
+++ frontend/src/pages/StudentSignup.jsx
@@ -1,36 +1,72 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../components/Header'
 import "../styles/StudentSignup.css"
 import "../styles/Form.css"
 
 
 function StudentSignup() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        fetch('http://localhost:3001/student/signup', {
+            method: "POST",
+            headers: {
+                "content-type": "application/json",
+            },
+            body: JSON.stringify({
+                name,
+                email,
+                password
+            })
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log(`Success: ${data}`);
+            })
+            .catch(error => console.error("Error sending data", error));
+
+        setName("");
+        setEmail("");
+        setPassword("");
+    }
+
+
 
     return (
         <div>
             <Header />
             <div className="form-container">
                 <h2>Student Signup</h2>
-                <form target='#'>
+                <form onSubmit={(e) => handleSubmit(e)}>
                     <div>
                         <label>Name:</label>
-                        <input 
-                            type="text" 
+                        <input
+                            type="text"
                             placeholder='Name'
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
                         />
                     </div>
                     <div>
                         <label>Email:</label>
-                        <input 
-                            type="email" 
+                        <input
+                            type="email"
                             placeholder='Email'
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
                         />
                     </div>
                     <div>
                         <label>Password:</label>
-                        <input 
-                            type="password" 
+                        <input
+                            type="password"
                             placeholder='Password'
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
                         />
                     </div>
                     <button type="submit">Get Started</button>
