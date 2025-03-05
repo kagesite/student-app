7ecae915d997523cbddd7602ba814226db9c4849
Kage Saluone <ksaluone17@gmail.com>
2025-03-05 08:49:14 -0700

    Created connection to database and can now send data to databasethrough the StudentSignup.jsx

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const bcrypt = require("bcrypt");
 const app = express();
 const PORT = process.env.PORT || 3001;
+const pool = require('./db');
 
 const path = require('path');
 
@@ -23,10 +25,31 @@ app.get('/', (req, res) => {
 })
 
 // Route for student signup
-app.post('/student/signup', (req, res) => {
+app.post('/student/signup', async (req, res) => {
     const { name, email, password } = req.body;
 
     console.log(`Name: ${name}, Email: ${email}, Password: ${password}`);
+
+
+    try {
+
+        const existingUser = await pool.query('SELECT * FROM testStu WHERE email = $1', [email]);
+        if (existingUser.rows.length > 0) {
+            return res.status(400).json({ message: "Email already in use" });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+
+        const newStudent = await pool.query(
+            'INSERT INTO testStu (name, email, password) VALUES ($1, $2, $3) RETURNING *', [name, email, hashedPassword]
+        )
+
+        res.status(201).json({ mesage: "Student registered sucessfully", user: newStudent.rows[0] });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "server error" });
+    }
 })
 
 
