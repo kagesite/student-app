bee98ed9805955620b86ab35fc32f52208894e9a
Kage Saluone <ksaluone17@gmail.com>
2025-03-04 22:05:44 -0700

    Created basic post for /student/signup that displays the form info in the console. Building stages still

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,9 @@ const path = require('path');
 // Have Node server the files for our built React app
 app.use(express.static(path.resolve(__dirname, "../client/dist")));
 
+app.use(cors())
+app.use(express.json());
+
 // Handle GET resuqest to /api route
 app.get('/api', (req, res) => {
     res.json({ message: "Hello from server!" });
@@ -18,6 +21,16 @@ app.get('/api', (req, res) => {
 app.get('/', (req, res) => {
     res.json({ message: "Server root page" });
 })
+
+// Route for student signup
+app.post('/student/signup', (req, res) => {
+    const { name, email, password } = req.body;
+
+    console.log(`Name: ${name}, Email: ${email}, Password: ${password}`);
+})
+
+
+
 
 app.listen(PORT, () => {
     console.log(`Server listening on port: ${PORT}`);
