1690aafa1aa972825622c039cd2d0231ec61e116
Tyler Kap <tylerkap55@gmail.com>
2025-03-06 11:17:25 -0700

    fixing and working on student update and delete endpoint

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, "../client/dist")));
 
-// Handle GET resuqest to /api route
+// Handle requests from client side
+
+// Creates a new student in the database | Returns a sucessful or unsucessful message
+// Todo: check if username is already in use
 app.post('/students/create', async (req, res) => {
 
     console.log(req.body);
@@ -37,15 +40,15 @@ app.post('/students/create', async (req, res) => {
 
         
         bcrypt.genSalt(10, (err, salt) => {
-            bcrypt.hash(process.env.SECRET_PASSWORD, salt, async (err, hash) => {
+            bcrypt.hash(password, salt, async (err, hash) => {
                 hashedPassword = hash;
 
                 const newStudent = await pool.query(
                     `INSERT INTO students (username, email, password, firstname, lastname, telephone, address)
-                    VALUES ($1, $2, $3, $4, $5, $6, $7)`, [username, email, hashedPassword, firstname, lastname, telephone, address]
+                    VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *`, [username, email, hashedPassword, firstname, lastname, telephone, address]
                 );
         
-                res.status(201).json({ message: "Student registered sucessfully"});
+                res.status(201).json({ message: "Student registered sucessfully", student: newStudent.rows[0]});
 
             });
         });
@@ -58,21 +61,24 @@ app.post('/students/create', async (req, res) => {
 });
 
 
-//Fixed: BCRYPT COMPARE IS NOT WORKING | Most answers online indicate it is because there is problem with typing in postgres or whitespace
-//Todo: add jwt to authorization and return token in an object with the success message
-//I thought I had to  use the unencrypted password of the user to compare, but the password you actually use is the one in the .env file. 
+// Todo: add jwt for authorization and return a token in an object with the success message
+// Todo: Check if they are either logging in with their email or username
 app.get('/students/login', async (req, res) => {
 
     let userCred = req.body;
 
     try {
         const existingUser = await pool.query('SELECT * FROM students WHERE email = $1', [userCred.email]);
+        const existingUserUsername = await pool.query('SELECT * FROM students WHERE username = $1', userCred.username);
 
-        if (existingUser.rows.length <= 0) {
+        if (existingUser.rows[0].email !== userCred.email) {
             return res.status(400).json({ message: "Incorrect Email"});
         }
+        else if (existingUser.rows[0].username !== userCred.username) {
+            return res.status(400).json({message: "Incorrect Username"})
+        }
         else {
-            bcrypt.compare(process.env.SECRET_PASSWORD, existingUser.rows[0].password, (err, result) => {
+            bcrypt.compare(userCred.password, existingUser.rows[0].password, (err, result) => {
                 console.log(result);
                 
                 if (result === true) {
@@ -94,7 +100,54 @@ app.get('/admins/login', (req, res) => {
 
 });
 
-app.get('/students/:email', async (req, res) => {
+// Searches database for a single student through the student_id search parameter | Returns found student object
+app.get('/students/id/:student_id', async (req, res) => {
+
+    const studentID = req.params.student_id;
+
+    try {
+
+        const existingUser = await pool.query('SELECT * FROM students WHERE student_id = $1', [studentID]);
+
+        if (existingUser.rows.length < 0) {
+            return res.status(400).json({ message: "Student not found"});
+        }
+
+        res.status(200).json(existingUser.rows[0]);    
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: "Server Error"});
+    }
+
+    
+});
+
+// Searches database for a single student through the username search parameter | Returns found student object
+app.get('/students/username/:username', async (req, res) => {
+
+    const username = req.params.username;
+
+    try {
+
+        const existingUser = await pool.query('SELECT * FROM students WHERE username = $1', [username]);
+
+        if (existingUser.rows.length < 0) {
+            return res.status(400).json({ message: "Student not found"});
+        }
+
+        res.status(200).json(existingUser.rows[0]);    
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: "Server Error"});
+    }
+
+    
+});
+
+// Searches database for a single student through the email search parameter | Returns found student object
+app.get('/students/email/:email', async (req, res) => {
 
     const student_email = req.params.email;
 
@@ -103,31 +156,63 @@ app.get('/students/:email', async (req, res) => {
         const existingUser = await pool.query('SELECT * FROM students WHERE email = $1', [student_email]);
 
         if (existingUser.rows.length < 0) {
-            return res.status(400).json({ message: "Email not Found"});
+            return res.status(400).json({ message: "Email/User not Found"});
         }
 
         res.status(200).json(existingUser.rows[0]);    
 
     } catch (error) {
         console.log(error);
-        res.status(500).json({ message: "Server Error"});
+        return res.status(500).json({ message: "Server Error"});
     }
 
     
 });
 
-app.put('/students/update/:email', (req, res) => {
+// Todo: check if updated username or email conflicts with already existing data in the database. There can be no duplicate usernames and emails.
+// Todo: If they are changing their password, you need to encrypt their new password.
+// Todo: Is there are better way of dynamically updating a student user without receving the whole object and updating everything?
+app.put('/students/update/:student_id', async (req, res) => {
+
+    const { username, email, password, firstname, lastname, telephone, address} = req.body;
+
+    if (password !== undefined) {
+
+
+
+    }
+    else {
+        res.status(400).json({message: "password is undefined"});
+    }
+
+
+    try {
+
+        const existingUser = await pool.query('UPDATE students SET username = $1, email = $2, password = $3, firstname = $4, lastname = $5, telephone = $6, address = $7 WHERE student_id = $8 RETURNING *', 
+            [username, email, password, firstname, lastname, telephone, address, req.params.student_id]);
+
+        if (existingUser.rows.length < 0) {
+            return res.status(400).json({ message: "Email/User not Found"});
+        }
+
+        res.status(200).json({message: "Update Succesful", student: existingUser.rows[0]});    
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: "Server Error"});
+    }
 
 });
 
-app.delete('/students/delete/:email', (req, res) => {
+app.delete('/students/delete/:student_id', (req, res) => {
 
 });
 
+// Returns an array of all available courses as course objects
 app.get('/courses', async (req, res) =>  {
     try {
 
-        const coursesArray = await pool.query('SELECT * FROM courses');
+        const coursesArray = await pool.query('SELECT * FROM courses ORDER BY course_id ASC');
         
         if (coursesArray.rows.length <= 0) {
             return res.status(400).json({message: "Courses Table Does NOT Exist"});
@@ -141,23 +226,24 @@ app.get('/courses', async (req, res) =>  {
     }
 });
 
-app.get('/students/courses/:email', (req, res) => {
+app.get('/students/courses/:student_id', (req, res) => {
 
 });
 
-app.put('/students/register?email=email&string_id=course', (req, res) => {
+app.put('/students/register?student_id=id&course_id=course', (req, res) => {
 
 });
 
-app.put('/students/unregister/email=student_email&string_id=course', (req, res) => {
+app.put('/students/unregister/student_id=id&course_id=course', (req, res) => {
 
 });
 
+// Returns an array of all created students as student objects
 app.get('/students', async (req, res) => {
 
     try {
 
-        const studentsArray = await pool.query('SELECT * FROM students');
+        const studentsArray = await pool.query('SELECT * FROM students ORDER BY student_id ASC');
         
         if (studentsArray.rows.length <= 0) {
             return res.status(400).json({message: "Student Table Does NOT Exist"});
@@ -175,11 +261,11 @@ app.post('/courses/create', (req, res) => {
 
 });
 
-app.put('/courses/update/:string_id', (req, res) => {
+app.put('/courses/update/:course_id', (req, res) => {
 
 });
 
-app.delete('/courses/delete/:string_id', (req, res) => {
+app.delete('/courses/delete/:course_id', (req, res) => {
 
 });
 
