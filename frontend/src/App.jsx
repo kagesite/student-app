1f66d97e59b7870babd138ced714c62033d0cfd6
Kage Saluone <ksaluone17@gmail.com>
2025-03-11 09:22:03 -0600

    Reverting a push

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,47 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import StudentSignup from './pages/StudentSignup';
-import StudentLogin from './pages/StudentLogin';
-import AdminLogin from './pages/AdminLogin';
-import StudentDash from './pages/StudentDash';
-import AdminDash from './pages/AdminDash';
-import "./styles/index.css"
-import StudentProfile from './pages/StudentProfile';
+// client/src/App.jsx
 
+import { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-    return (
-        <Routes>
-            <Route path="/" element={<StudentSignup />} />
-            <Route path="student-login" element={<StudentLogin />} />
-            <Route path="admin-login" element={<AdminLogin />} />
-            <Route path="student-dash" element={<StudentDash />} />
-            <Route path="admin-dash" element={<AdminDash />} />
-            <Route path="student-profile" element={<StudentProfile /> } />
-        </Routes>
-    )
+  const [count, setCount] = useState(0);
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+         <h1>{!data ? "Loading..." : data}</h1>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
 export default App
+
