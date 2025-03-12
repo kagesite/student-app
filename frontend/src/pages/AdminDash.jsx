c021be4cd35f9755fa62ea75c05923d398d7e92f
Kage Saluone <ksaluone17@gmail.com>
2025-03-12 08:03:56 -0600

    Started creating the admin dashboard. Doing styling and logic too

--- frontend/src/pages/AdminDash.jsx
+++ frontend/src/pages/AdminDash.jsx
@@ -1,11 +1,77 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from '../components/Header'
 import TestFooter from '../components/TestFooter'
+import '../styles/AdminDash.css'
 
 function AdminDash() {
+    const [courses, setCourses] = useState([]);
+    const [selectedCourse, setSelectedCourse] = useState(null);
+    const [students, setStudents] = useState([]);
+    const [isCoursesModalOpen, setIsCoursesModalOpen] = useState(false);
+
+    useEffect(() => {
+        fetch('http://localhost:3001/courses')
+            .then(response => response.json())
+            .then(data => setCourses(data));
+    })
+
+    const showCoursesModal = (id) => {
+        const selectedCourse = courses.find(c => c.course_id === id);
+        setSelectedCourse(selectedCourse);
+        setIsCoursesModalOpen(true);
+    }
+
+    const closeCourseModal = () => {
+        setIsCoursesModalOpen(false);
+    }
+
+
+
     return (
         <div>
             <Header />
+            <main>
+                <h1 className='page-title'>Admin Dashboard</h1>
+                <div className="admin-container">
+                    <div className="courses-container">
+                        <h2>All Courses</h2>
+                        <ul className='courses-list'>
+                            {courses.map(course => {
+                                return (
+                                    <li key={course.course_id} className='course-item'>
+                                        <h3>{course.title}</h3>
+                                        <button onClick={() => showCoursesModal(course.course_id)}>Details</button>
+                                    </li>
+                                )
+                            })}
+                        </ul>
+                    </div>
+                    <div className="students-container">
+                        test
+                    </div>
+                </div>
+
+                {isCoursesModalOpen && (
+                    <div className="modal-overlay">
+                        <div className="modal">
+                            <div className="modal-top">
+                                <button className="close-modal" onClick={closeCourseModal}>X</button>
+                            </div>
+                            <div className='modal-inside'>
+                                <h2>{selectedCourse.title}</h2>
+                                <p><strong>Class ID:</strong> {selectedCourse.string_id}</p>
+                                <p><strong>Description:</strong> {selectedCourse.description}</p>
+                                <p><strong>Schedule:</strong> {selectedCourse.schedule}</p>
+                                <p><strong>Location:</strong> {selectedCourse.classroom_number}</p>
+                                <p><strong>Capacity:</strong> {selectedCourse.maximum_capacity}</p>
+                                <p><strong>Credits:</strong> {selectedCourse.credit_hours}</p>
+                                <p><strong>Fee:</strong> ${selectedCourse.tuition_cost}</p>
+                                <button className="registered-students-btn" >Registered Students</button>
+                            </div>
+                        </div>
+                    </div>
+                )}
+            </main>
             <TestFooter />
         </div>
     )
